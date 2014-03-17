236ad03bc2793ae4362fed5eaccbc8445253f7fa
Mitchell Mason <thefreemason11@gmail.com>
2014-03-17 13:14:16 -0500

    Heavy documenting done, along with some work in traversing the test1.tig. Still working!

--- chap5/Semant/Env.java
+++ chap5/Semant/Env.java
@@ -1,4 +1,5 @@
 package Semant;
+
 import Symbol.Table;
 import Symbol.Symbol;
 import Types.Type;
@@ -6,54 +7,52 @@ import Types.RECORD;
 import Types.NAME;
 
 class Env {
-  Table venv;			// value environment
-  Table tenv;			// type environment
-  ErrorMsg.ErrorMsg errorMsg;
+	Table				venv;		// value environment
+	Table				tenv;		// type environment
+	ErrorMsg.ErrorMsg	errorMsg;
 
-  private static Symbol sym(String s) {
-    return Symbol.symbol(s);
-  }
+	private static Symbol sym(String s) {
+		return Symbol.symbol(s);
+	}
 
-  private static final Types.VOID VOID = Semant.VOID;
-  private static RECORD RECORD(Symbol n, Type t, RECORD x) {
-    return new RECORD(n, t, x);
-  }
-  private static RECORD RECORD(Symbol n, Type t) {
-    return new RECORD(n, t, null);
-  }
+	private static final Types.VOID	VOID	= Semant.VOID;
 
-  private static FunEntry FunEntry(RECORD f, Type r) {
-    return new FunEntry(f, r);
-  }
+	private static RECORD RECORD(Symbol n, Type t, RECORD x) {
+		return new RECORD(n, t, x);
+	}
 
-  Env(ErrorMsg.ErrorMsg err) {
-    errorMsg = err;
-    venv = new Table();
-    tenv = new Table();
+	private static RECORD RECORD(Symbol n, Type t) {
+		return new RECORD(n, t, null);
+	}
 
-    // initialize tenv and venv with predefined identifiers
-    NAME INT = new NAME(sym("int"));
-    INT.bind(Semant.INT);
-    tenv.put(sym("int"), INT);
+	private static FunEntry FunEntry(RECORD f, Type r) {
+		return new FunEntry(f, r);
+	}
 
-    NAME STRING = new NAME(sym("string"));
-    STRING.bind(Semant.STRING);
-    tenv.put(sym("string"), STRING);
+	Env(ErrorMsg.ErrorMsg err) {
+		errorMsg = err;
+		venv = new Table();
+		tenv = new Table();
 
-    venv.put(sym("print"),     FunEntry(RECORD(sym("s"), STRING), VOID));
-    venv.put(sym("flush"),     FunEntry(null, VOID));
-    venv.put(sym("getchar"),   FunEntry(null, STRING));
-    venv.put(sym("ord"),       FunEntry(RECORD(sym("s"), STRING), INT));
-    venv.put(sym("chr"),       FunEntry(RECORD(sym("i"), INT), STRING));
-    venv.put(sym("size"),      FunEntry(RECORD(sym("s"), STRING), INT));
-    venv.put(sym("substring"), FunEntry(RECORD(sym("s"), STRING,
-					       RECORD(sym("first"), INT,
-						      RECORD(sym("n"), INT))),
-					STRING));
-    venv.put(sym("concat"),    FunEntry(RECORD(sym("s1"), STRING,
-					       RECORD(sym("s2"), STRING)),
-					STRING));
-    venv.put(sym("not"),       FunEntry(RECORD(sym("i"), INT), INT));
-    venv.put(sym("exit"),      FunEntry(RECORD(sym("i"), INT), VOID));
-  }
+		// initialize tenv and venv with predefined identifiers
+		NAME INT = new NAME(sym("int"));
+		INT.bind(Semant.INT);
+		tenv.put(sym("int"), INT);
+
+		NAME STRING = new NAME(sym("string"));
+		STRING.bind(Semant.STRING);
+		tenv.put(sym("string"), STRING);
+
+		venv.put(sym("print"), FunEntry(RECORD(sym("s"), STRING), VOID));
+		venv.put(sym("flush"), FunEntry(null, VOID));
+		venv.put(sym("getchar"), FunEntry(null, STRING));
+		venv.put(sym("ord"), FunEntry(RECORD(sym("s"), STRING), INT));
+		venv.put(sym("chr"), FunEntry(RECORD(sym("i"), INT), STRING));
+		venv.put(sym("size"), FunEntry(RECORD(sym("s"), STRING), INT));
+		venv.put(sym("substring"),
+				FunEntry(RECORD(sym("s"), STRING, RECORD(sym("first"), INT, RECORD(sym("n"), INT))), STRING));
+		venv.put(sym("concat"), FunEntry(RECORD(sym("s1"), STRING, RECORD(sym("s2"), STRING)), STRING));
+		venv.put(sym("not"), FunEntry(RECORD(sym("i"), INT), INT));
+		venv.put(sym("exit"), FunEntry(RECORD(sym("i"), INT), VOID));
+	}
 }
--- chap5/Semant/Semant.java
+++ chap5/Semant/Semant.java
@@ -4,53 +4,59 @@ import Translate.Exp;
 import Types.Type;
 
 public class Semant {
-	Env		env;
-	boolean	debug	= false;
+	Env							env;
+	boolean						debug	= false;
+
+	// Types to check against
+	static final Types.VOID		VOID	= new Types.VOID();
+	static final Types.INT		INT		= new Types.INT();
+	static final Types.STRING	STRING	= new Types.STRING();
+	static final Types.NIL		NIL		= new Types.NIL();
 
 	public Semant(ErrorMsg.ErrorMsg err, boolean debug) {
-		this(new Env(err)); // This is lazy but should work. I couldn't call Semant(ErrorMsg.ErrorMsg) for whatever reason
+		this(new Env(err));
 		this.debug = debug;
 	}
-
-	// public Semant(ErrorMsg.ErrorMsg err) {
-	// this(new Env(err));
-	// }
 
 	Semant(Env e) {
 		env = e;
 	}
 
 	/**
-	 * Every project I've ever done for these classes benefitted enormously from this method.
+	 * Every project I've ever done for these classes benefitted enormously from this method. We pass a message and an object (for clarity.) If the messages
+	 * comes from the containing class, we'll draw a big section marker with the message.
 	 */
-	public void debugPrint(Object sender, String message) {
-		if (debug)
-			{
-				System.out.println(sender.getClass().toString() + "-> " + message);
+	private void debugPrint(Object sender, String message) {
+		if (debug) {
+			if (!(sender instanceof Semant)) {
+				System.out.println(sender.getClass().toString().substring(6) + "-> " + message);
 			}
+			else {
+				System.out.println("\n*************** " + message.toUpperCase() + " ***************");
+			}
+		}
 	}
 
+	/**
+	 * Used from the Main class to begin Semantic analysis.
+	 */
 	public void transProg(Absyn.Exp exp) {
-		debugPrint(this, "Beginning parsing with exp: " + exp.toString());
-		try
-			// If we need to, this allows us the room to print the symbol table on error catching
-			{
-				transExp(exp);
-			} catch (Error e)
-			{
-				System.err.println(e);
-				// Print the symbol table?
-			}
+		debugPrint(this, "Begin!");
+		// If we need to, this allows us the room to print the symbol table on error catching
+		try {
+			debugPrint(exp, "Start decent of expression.");
+			transExp(exp);
+			debugPrint(this, "Done!"); // eyes on the prize!
+		}
+		catch (Error e) {
+			System.err.println(e);
+			// Print the symbol table? -Not sure how to do.
+		}
 	}
 
 	private void error(int pos, String msg) {
 		env.errorMsg.error(pos, msg);
 	}
-
-	static final Types.VOID		VOID	= new Types.VOID();
-	static final Types.INT		INT		= new Types.INT();
-	static final Types.STRING	STRING	= new Types.STRING();
-	static final Types.NIL		NIL		= new Types.NIL();
 
 	private Exp checkInt(ExpTy et, int pos) {
 		if (!INT.coerceTo(et.ty))
@@ -58,22 +64,85 @@ public class Semant {
 		return et.exp;
 	}
 
+	/**
+	 * The main switchboard for transExps. We take a general expression, discover its form, and pass it along.
+	 */
 	ExpTy transExp(Absyn.Exp e) {
-		ExpTy result;
+		debugPrint(this, "Traversing General Expression " + e);
+		debugPrint(e, "First, we determine the type of the expression.");
+		ExpTy result = null;
 
-		if (e == null)
+		if (e == null) {
+			debugPrint(e, "e is null.");
 			return new ExpTy(null, VOID);
-		else if (e instanceof Absyn.OpExp)
-			{
-				result = transExp((Absyn.OpExp) e);
-				debugPrint(e, "I'm an OpExp!");
-			} else if (e instanceof Absyn.LetExp)
-			{
-				result = transExp((Absyn.LetExp) e);
-				debugPrint(e, "I'm an LetExp!");
-			} else
-			throw new Error("First expression must be an opexp or letexp");
+		}
+		else if (e instanceof Absyn.OpExp) {
+			debugPrint(e, "I'm an OpExp!");
+			result = transExp((Absyn.OpExp) e);
+		}
+		else if (e instanceof Absyn.LetExp) {
+			debugPrint(e, "I'm a LetExp!");
+			result = transExp((Absyn.LetExp) e);
+		}
+		else if (e instanceof Absyn.ArrayExp) {
+			debugPrint(e, "I'm an ArrayExp!");
+			result = transExp((Absyn.ArrayExp) e);
+		}
+		else if (e instanceof Absyn.AssignExp) {
+			debugPrint(e, "I'm an AssignExp!");
+			result = transExp((Absyn.AssignExp) e);
+		}
+		else if (e instanceof Absyn.BreakExp) {
+			debugPrint(e, "I'm a BreakExp!");
+			result = transExp((Absyn.BreakExp) e);
+		}
+		else if (e instanceof Absyn.CallExp) {
+			debugPrint(e, "I'm a CallExp!");
+			result = transExp((Absyn.CallExp) e);
+		}
+		else if (e instanceof Absyn.ForExp) {
+			debugPrint(e, "I'm a ForExp!");
+			result = transExp((Absyn.ForExp) e);
+		}
+		else if (e instanceof Absyn.IfExp) {
+			debugPrint(e, "I'm an IfExp!");
+			result = transExp((Absyn.IfExp) e);
+		}
+		else if (e instanceof Absyn.IntExp) {
+			debugPrint(e, "I'm an IntExp!");
+			result = transExp((Absyn.IntExp) e);
+		}
+		else if (e instanceof Absyn.NilExp) {
+			debugPrint(e, "I'm a NilExp!");
+			result = transExp((Absyn.NilExp) e);
+		}
+		else if (e instanceof Absyn.RecordExp) {
+			debugPrint(e, "I'm a RecordExp!");
+			result = transExp((Absyn.RecordExp) e);
+		}
+		else if (e instanceof Absyn.SeqExp) {
+			debugPrint(e, "I'm a SeqExp!");
+			result = transExp((Absyn.SeqExp) e);
+		}
+		else if (e instanceof Absyn.StringExp) {
+			debugPrint(e, "I'm a StringExp!");
+			result = transExp((Absyn.StringExp) e);
+		}
+		else if (e instanceof Absyn.VarExp) {
+			debugPrint(e, "I'm a VarExp!");
+			result = transExp((Absyn.VarExp) e);
+		}
+		else if (e instanceof Absyn.WhileExp) {
+			debugPrint(e, "I'm a WhileExp!");
+			result = transExp((Absyn.WhileExp) e);
+		}
+		else
+			error(((Absyn.Absyn) e).pos, "Unsure what type of expression we're dealing with here...");
+		if (result == null) {
+			debugPrint(e, "the result from this transExp is null. Likely that we haven't implemented it yet.");
+		}
 		e.type = result.ty;
+		debugPrint(this, "expression " + e.toString() + " evaluated to " + result.toString());
 		return result;
 	}
 
@@ -81,6 +150,7 @@ public class Semant {
 		ExpTy left = transExp(e.left);
 		ExpTy right = transExp(e.right);
 
+		// TODO implement the other operations(?)
 		switch (e.oper) {
 			case Absyn.OpExp.PLUS:
 				checkInt(left, e.left.pos);
@@ -91,59 +161,208 @@ public class Semant {
 		}
 	}
 
+	/**
+	 * I'm pretty sure there should only be 1 let statement. Should we check for that?
+	 */
 	ExpTy transExp(Absyn.LetExp e) {
-		debugPrint(e, "Beginning traversal of a Let expr");
-		env.venv.beginScope();
-		env.tenv.beginScope();
-		for (Absyn.DecList d = e.decs; d != null; d = d.tail)
-			{
-				transDec(d.head);
-			}
+		debugPrint(this, "Beginning traversal of a Let expr");
+		env.venv.beginScope(); // venv should be value environment
+		env.tenv.beginScope(); // tenc should be type environment
+		debugPrint(e, "Began a new val env and type env.");
+
+		// Iterate through the declist of this let expr
+		for (Absyn.DecList d = e.decs; d != null; d = d.tail) {
+			debugPrint(d, "Beginning new transDec of the head: " + d.head.toString());
+			transDec(d.head);
+			debugPrint(this, "continuing Let expr");
+		}
 		ExpTy body = transExp(e.body);
 		env.venv.endScope();
 		env.tenv.endScope();
 		return new ExpTy(null, body.ty);
 	}
 
-	Exp transDec(Absyn.Dec d) {
-		debugPrint(this, "Beginning transDec with Dec " + d.toString());
-		if (d instanceof Absyn.VarDec)
-			{
-				Absyn.VarDec dee = (Absyn.VarDec) d;
-				debugPrint(dee, "I'm a VarDec with type:" + dee.typ.toString());
-				/* From the book, pg 124 */
-				VarEntry ve = new VarEntry(transExp(dee.init).ty);
-				env.venv.put(dee.name, ve);
-				debugPrint(d, "Added a new var to the table: " + ve.toString());
-				return transDec(dee);
-				/* end book code */
-				//return transDec((Absyn.VarDec) d);
+	// TODO implement
+	ExpTy transExp(Absyn.ArrayExp e) {
+		debugPrint(this, "Traversing ArrayExp...");
+		debugPrint(e, "Get the NAME of the expression from the type env.");
+
+		Types.NAME name = (Types.NAME) env.tenv.get(e.typ);
+
+		debugPrint(name, "Now we evalutate it to see if the NAME is an ARRAY.");
+		if (name == null) {
+			debugPrint(e, "The type of the array hasn't been declared yet. DUCK!!!");
+			error(((Absyn.Absyn) (e)).pos, "undeclared type: " + e.typ);
+		}
+		else {
+			debugPrint(name, "We found the name in the tenv. So far so good.");
+		}
+
+		debugPrint(name, "name is: " + name);
+		debugPrint(e, "Get the size of the array by traversing the IntExpr.");
+		ExpTy size = transExp(e.size);
+
+		debugPrint(this, "continuing with transExp(ArrayExp).");
+		debugPrint(size, "Let's make sure this is actually an int.");
+		checkInt(size, ((Absyn.Absyn) e).pos);
+
+		debugPrint(e, "now, we run the init part of the expression.");
+		ExpTy init = transExp(e.init);
+		debugPrint(this, "continuing with transExp(ArrayExp).");
+		debugPrint(e, "Last, we check to be sure the array's type and the elements' type match up.");
+		
+		System.out.println(name.actual());
+		Type actual = name.actual(); //This got explained in class.
+		if (actual instanceof Types.ARRAY) {
+			Types.ARRAY array = (Types.ARRAY) actual;
+			if (!init.ty.coerceTo(array.element)) {
+				debugPrint(init, "type of init and type of array don't match up.");
+				error(((Absyn.Absyn) (e.init)).pos, "element type mismatch");
 			}
-		else if(d instanceof Absyn.TypeDec){ //Added by Mitch.
-			//env.tenv.put(d,)
-			return null; //TODO fix this!
-		} //</Mitch>
-		throw new Error("Dec supplied is not a varDec.");
+			else {
+				return new ExpTy(null, name);
+			}
+		}
+
+		return new ExpTy(null, VOID);
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.AssignExp e) {
+		debugPrint(this, "Traversing AssignExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.BreakExp e) {
+		debugPrint(this, "Traversing BreakExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.CallExp e) {
+		debugPrint(this, "Traversing CallExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.IfExp e) {
+		debugPrint(this, "Traversing IfExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.IntExp e) {
+		debugPrint(this, "Traversing IntExp...");
+		return new ExpTy(null, INT);
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.NilExp e) {
+		debugPrint(this, "Traversing NilExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.RecordExp e) {
+		debugPrint(this, "Traversing RecordExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.SeqExp e) {
+		debugPrint(this, "Traversing IfExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.StringExp e) {
+		debugPrint(this, "Traversing StringExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.VarExp e) {
+		debugPrint(this, "Traversing VarExp...");
+		return null;
+	}
+
+	// TODO implement
+	ExpTy transExp(Absyn.WhileExp e) {
+		debugPrint(this, "Traversing WhileExp...");
+		return null;
 	}
 
 	/**
-	 * This is the meat of the program. Given a variable declaration like: a =: 2 determine the type O.o
+	 * This method exists to hand a general declaration. We figure out it's type, and then move to the appropriate method with a more specific decaraltion type.
+	 */
+	Exp transDec(Absyn.Dec d) {
+		debugPrint(this, "Beginning transDec with Dec " + d.toString());
+		debugPrint(d, "Let's determine the subtype.");
+		if (d instanceof Absyn.VarDec) {
+			debugPrint(d, "I'm a VarDec; traversing");
+			return transDec((Absyn.VarDec) d);
+		}
+		else if (d instanceof Absyn.TypeDec) { // Added by Mitch.
+			debugPrint(d, "I'm a TypeDec; traversing");
+			return transDec((Absyn.TypeDec) d);
+		}
+		else if (d instanceof Absyn.FunctionDec) { // Added by Mitch.
+			debugPrint(d, "I'm a FunctionDec; traversing");
+			return transDec((Absyn.FunctionDec) d);
+		}
+		else
+			throw new Error("Dec supplied is not actually a dec or isn't implemented");
+	}
+
+	// TODO implement
+	Exp transDec(Absyn.TypeDec d) {
+		debugPrint(this, "Traversing typedec");
+		debugPrint(d, "Typedecs have many symbols. Let's build a list of them");
+		java.util.ArrayList<Symbol.Symbol> symList = new java.util.ArrayList<Symbol.Symbol>();
+		for (Absyn.TypeDec head = d; head != null; head = head.next) {
+			// ArrayList.add() will return false if the element is already present in the list.
+			if (symList.add(head.name)) {
+				env.tenv.put(head.name, new Types.NAME(head.name));
+				debugPrint(head, "added " + head.name + " to the types tables");
+			}
+			else {
+				debugPrint(head, "duplicate name detected!");
+				throw new Error("Duplicate name detected in type declaration.");
+			}
+		}
+
+		return null;
+	}
+
+	// TODO implement
+	Exp transDec(Absyn.FunctionDec d) {
+		debugPrint(this, "Traversing functiondec");
+		return null;
+	}
+
+	/**
+	 * It appears we need a transDec method for each and every form of Absyn.Dec
 	 */
 	Exp transDec(Absyn.VarDec d) {
-		// NOTE: THIS IMPLEMENTATION IS INCOMPLETE
-		// It is here to show you the general form of the transDec methods
+		debugPrint(d, "Beggining transDec");
+
+		// Get the Expression type (a class composed of an expression, and it's resulting type.)
 		ExpTy init = transExp(d.init);
-		Type type;
-		if (d.typ == null)
-			{
-				type = init.ty;
-			}
-		// else if(d.typ instanceof Something){debugPrint(d,"I'm a Something");}
-		else
-			{
-				type = VOID;
-				throw new Error("unimplemented");
-			}
+		Type type; // we determine the type in the if statement below
+		if (d.typ == null) {
+			debugPrint(d, "my typ is null, pull my real type from the init data I hold.");
+			type = init.ty; // If the VarDec doesn't have a type stored yet, pull it from itself.
+			debugPrint(d, "That type is: " + type.toString());
+		}
+		else {
+			type = VOID;
+			throw new Error("unimplemented"); // Not sure what to implement here... It sounds like a complete logic flow to me -Mitch
+												// Maybe we're this occurs when a varible has already been declared? -Mitch
+		}
+
+		// Store the resulting entry
+		debugPrint(d, "Storing in the env table.");
 		d.entry = new VarEntry(type);
 		env.venv.put(d.name, d.entry);
 		return null;
