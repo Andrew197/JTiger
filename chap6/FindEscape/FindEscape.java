209f7a508b48b220e484693e651f7c22ba214c3a
Mitchell Mason <thefreemason11@gmail.com>
2014-03-26 12:35:31 -0500

    Added the necessary files and classes to begin Project 4. Also, in the folder ref/ is the reference project to test against. I've also added all the needed method stubs suggested in the project description.

---
+++ chap6/FindEscape/FindEscape.java
@@ -0,0 +1,106 @@
+package FindEscape;
+
+import Absyn.*;
+
+public class FindEscape {
+	Symbol.Table	escEnv	= new Symbol.Table();	// escEnv maps Symbol to Escape
+	static final boolean debug = true;
+
+	public FindEscape(Exp e) {
+		traverseExp(0, e);
+	}
+
+	/**
+	 * Every project I've ever done for these classes benefitted enormously from this method. We pass a message and an object (for clarity.) If the messages
+	 * comes from the containing class, we'll draw a big section marker with the message.
+	 */
+	private void debugPrint(Object sender, String message) {
+		if (debug) {
+			if (sender == null) {
+				System.out.println("null -> " + message); // Null ptr exeception protection.
+			}
+			if (!(sender instanceof FindEscape)) {
+				System.out.println(sender.getClass().toString().substring(6) + "-> " + message);
+			}
+			else {
+				System.out.println("\n*************** " + message.toUpperCase() + " ***************");
+			}
+		}
+	}
+
+	/**
+	 * acts as a switch statement for subclasses of Absyn.Var
+	 */
+	void traverseVar(int depth, Var v) {
+		if (v instanceof SimpleVar)
+			traverseVar(depth, (SimpleVar) v);
+		else if (v instanceof FieldVar)
+			traverseVar(depth, (FieldVar) v);
+		else if (v instanceof SubscriptVar)
+			traverseVar(depth, (SubscriptVar) v);
+		else
+			debugPrint(v, "Is not a subclass of Var, no method to handle.");
+	}
+	
+	void traverseVar(int depth, SimpleVar v) {}
+	void traverseVar(int depth, FieldVar v) {}
+	void traverseVar(int depth, SubscriptVar v) {}
+
+	/**
+	 * acts as a switch statement for subclasses of Absyn.Exp
+	 */
+	void traverseExp(int depth, Exp e) {
+		if (e instanceof VarExp)
+			traverseExp(depth, (VarExp) e);
+		else if (e instanceof CallExp)
+			traverseExp(depth, (CallExp) e);
+		else if (e instanceof OpExp)
+			traverseExp(depth, (OpExp) e);
+		else if (e instanceof RecordExp)
+			traverseExp(depth, (RecordExp) e);
+		else if (e instanceof SeqExp)
+			traverseExp(depth, (SeqExp) e);
+		else if (e instanceof AssignExp)
+			traverseExp(depth, (AssignExp) e);
+		else if (e instanceof IfExp)
+			traverseExp(depth, (IfExp) e);
+		else if (e instanceof WhileExp)
+			traverseExp(depth, (WhileExp) e);
+		else if (e instanceof ForExp)
+			traverseExp(depth, (ForExp) e);
+		else if (e instanceof LetExp)
+			traverseExp(depth, (LetExp) e);
+		else if (e instanceof ArrayExp)
+			traverseExp(depth, (ArrayExp) e);
+		else
+			debugPrint(e, "Is not a subclass of Exp, no method to handle.");
+	}
+	
+	void traverseExp(int depth, VarExp e) {}
+	void traverseExp(int depth, CallExp e) {}
+	void traverseExp(int depth, OpExp e) {}
+	void traverseExp(int depth, RecordExp e) {}
+	void traverseExp(int depth, SeqExp e) {}
+	void traverseExp(int depth, AssignExp e) {}
+	void traverseExp(int depth, IfExp e) {}
+	void traverseExp(int depth, WhileExp e) {}
+	void traverseExp(int depth, ForExp e) {}
+	void traverseExp(int depth, LetExp e) {}
+	void traverseExp(int depth, ArrayExp e) {}
+	
+
+	/**
+	 * acts as a switch statement for subclasses of Absyn.Dec
+	 */
+	void traverseDec(int depth, Dec d) {
+		if (d instanceof VarDec)
+			traverseDec(depth, (VarDec) d);
+		else if (d instanceof FunctionDec)
+			traverseDec(depth, (FunctionDec) d);
+		else
+			debugPrint(d, "Is not a subclass of Dec, no method to handle.");
+	}
+	
+	void traverseExp(int depth, VarDec d) {}
+	void traverseExp(int depth, FunctionDec d) {}
+}
